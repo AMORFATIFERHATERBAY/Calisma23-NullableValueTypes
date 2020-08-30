fcba5e5186c53ed802a4c4c8f631864c874dda7b
AmorFatiBA <you@example.com>
2020-08-30 14:21:40 +0300

    first commit

---
+++ Nullable.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Calisma23_NullableValueTypes
+{
+    public class Nullable
+    {
+        public struct Nullable1<T> where T : struct
+        {
+            private T value;
+            // private bool hasValue;
+
+            public T Value
+            {
+                get
+                {
+                    return value;
+                }
+                set
+                {
+                    this.value = value;
+                }
+            }
+            // public bool HasValue
+            // {
+            //     get;
+               
+            // }
+            // public T GetValueOrDefault()
+            // {
+                
+            // }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+
+namespace Calisma23_NullableValueTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Nullable<int> n = new Nullable<int>();
+            n = 5;
+           Console.WriteLine(n.GetValueOrDefault());
+           Nullable<double> d = null;
+           Nullable<double> d1 = 10.10;
+           Console.WriteLine(d);
+           Console.WriteLine(d.GetValueOrDefault());
+           Console.WriteLine(d1);
+           Console.WriteLine(d1.GetValueOrDefault());
+
+        }
+    }
+}
